248d95f1c7e089f71e76f36cdcfa299ebe07d803
Edwin1115 <163002162+nanocd345@users.noreply.github.com>
2024-05-25 23:42:51 +0000

    feat: :bug: arreglo de API

--- src/js/modules/getData.js
+++ src/js/modules/getData.js
@@ -1,6 +1,6 @@
 export const getDataAllData = async () => {
     try {
-        let res = await fetch('https://file.notion.so/f/f/eaa1771c-fc19-40d4-8527-37ca1caab8fa/8f181ea0-47f7-49a5-9b85-48db35d8ec38/Documentos_DB.json?id=a21b973c-4a2b-4e71-b3f3-1b6e38a01f05&table=block&spaceId=eaa1771c-fc19-40d4-8527-37ca1caab8fa&expirationTimestamp=1716580800000&signature=DMJhF5QxwlisUz-qmwE4PxMl_oFHgG-vvZVM5pH9TNQ&downloadName=Documentos_DB.json');
+        let res = await fetch('https://file.notion.so/f/f/eaa1771c-fc19-40d4-8527-37ca1caab8fa/8f181ea0-47f7-49a5-9b85-48db35d8ec38/Documentos_DB.json?id=a21b973c-4a2b-4e71-b3f3-1b6e38a01f05&table=block&spaceId=eaa1771c-fc19-40d4-8527-37ca1caab8fa&expirationTimestamp=1716768000000&signature=DmNHOucnGkfcgGJUa3Rl0m_lgSc-SZXaEuR9YnCVYZA&downloadName=Documentos_DB.json');
         if (res.ok) {
             const data = await res.json();
             return data;
